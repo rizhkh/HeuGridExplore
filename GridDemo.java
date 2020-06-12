2a15446cc6d53c20a310ab49a0eae101658c8b17
Rizwan <mrk_rizwan@hotmail.com>
2020-06-11 21:53:34 -0400

    Update GridDemo.java

--- GridDemo.java
+++ GridDemo.java
@@ -1,40 +1,26 @@
-
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
 public class GridDemo {
 
-public static void main(String[] args)
-{
-
-	boolean check=true;
-	grid g = new grid();
-	Scanner scan = new Scanner(System.in);
-	g.direction();
-	g.gridInitializer();
-	//System.out.print("Enter 1 to move left, 5 to up,3 to move right,2 to move down:");
-    int input ;//= scan.nextInt();
-	//g.gridInitialDirection(input);
-	//g.gridMap();
-	System.out.println();
-	g.gridBlockAdd(); //////////////////////////////////////////////////////////////////////////////////////////////////////////
-			g.gridMap1();
-	do
-	{
-
-		g.gridMap1();
-		//System.out.print("Enter 1 to move left, 5 to up,3 to move right,2 to move down:");
-    	//input = scan.nextInt();
-		check = g.gridMover();
-		//g.gridMap();
+	public static void main(String[] args){
+		boolean check=true;
+		grid gridObj = new grid();
+		Scanner scan = new Scanner(System.in);
+		gridObj.direction();
+		gridObj.gridInitializer();
+		int input ;
+		System.out.println();
+		gridObj.gridBlockAdd();
+		gridObj.gridMap1();
+		do
+		{
+			gridObj.gridMap1();
+			check = gridObj.gridMover();
+		}
+		while(check==true);
+		gridObj.gridMap1();
 	}
-	while(check==true);
-
-			g.gridMap1();
-	//g.closed();
-
-}
-
 
 }
